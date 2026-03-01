3c192e76e6a80947463e4e79dc44908785c951e9
nelsw <connorvanelswyk@gmail.com>
2026-03-01 00:36:22 -0500

    make the error page tasteful

--- src/pages/ErrorPage.vue
+++ src/pages/ErrorPage.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-dark text-white text-center q-pa-sm flex flex-center">
     <div>
-      <div style="font-size: 30vh">404</div>
-
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <img
+        src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2g2MzZ6a2wxd2IwcTZ5czM4NTlxOGphenVvcjh2anlvZ2wxYWUwMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/guSDEekNFb23C/giphy.gif"
+        alt="404"
+        style="max-width: 350px;"
+      />
+      <div class="text-h3 q-my-xl" style="opacity: 0.4">
+        If you 🍟 when you should 🍕,
+        <br />
+        you're gonna have a bad time.
+      </div>
 
       <q-btn
-        class="q-mt-xl"
+        class=""
         color="white"
         text-color="blue"
         unelevated
         to="/"
-        label="Go Home"
+        label="Let's go 🏠 mkay?"
         no-caps
+        size="lg"
       />
     </div>
   </div>
