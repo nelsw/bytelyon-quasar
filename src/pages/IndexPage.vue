a2f73a0740c5d01d9948b23813f86bf11f73d307
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2026-03-30 16:31:42 -0400

    rip useless buttons

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,9 +6,6 @@ import HomePage from 'pages/HomePage.vue';
 import BotPage from 'pages/BotPage.vue';
 import type { BotNode } from 'src/types/model';
 import { BotType } from 'src/types/model';
-import EmailBtn from 'components/btn/EmailBtn.vue';
-import LogoutBtn from 'components/btn/LogoutBtn.vue';
-import SettingsBtn from 'components/btn/SettingsBtn.vue';
 import NewsPage from 'pages/NewsPage.vue';
 import SitemapPage from 'pages/SitemapPage.vue';
 import SearchPage from 'pages/SearchPage.vue';
@@ -44,18 +41,8 @@ const onDeleted = () => (bot.value = undefined);
             </div>
           </div>
           <q-separator />
-
-          <div style="height: calc(100vh - 102px)">
+          <div style="height: calc(100vh - 56px)">
             <BotTree v-model:selected="selected" @update:bot="onUpdateBot" />
-          </div>
-
-          <div class="absolute-bottom bg-dark">
-            <q-separator />
-            <q-btn-group push spread flat square>
-              <LogoutBtn />
-              <EmailBtn />
-              <SettingsBtn />
-            </q-btn-group>
           </div>
         </div>
       </template>
