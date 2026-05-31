7ee32a268453b6af2f44e83175eef7abc57431ab
nelsw <connorvanelswyk@gmail.com>
2026-05-31 10:22:43 -0400

    persist to session

--- src/stores/bots.ts
+++ src/stores/bots.ts
@@ -13,60 +13,69 @@ type Model = Record<BotType, Bots>;
 const $api = useBotApi();
 const $notify = useNotifier();
 
-export const useBotStore = defineStore('bots', () => {
-  const model = ref<Model>({} as Model);
-  const save = async (bot: Bot, create :boolean) => {
-    console.debug('saving...', bot);
-    const v = Validate(bot);
-    if (v.length > 0) {
-      $notify.warn(v);
-      return;
-    }
+export const useBotStore = defineStore(
+  'bots',
+  () => {
+    const model = ref<Model>({} as Model);
+    const save = async (bot: Bot, create: boolean) => {
+      console.debug('saving...', bot);
+      const v = Validate(bot);
+      if (v.length > 0) {
+        $notify.warn(v);
+        return;
+      }
 
-    const msg = `${capitalize(bot.type)} Bot ${create ? 'Created' : 'Updated'}`;
-    try {
-      const bots = model.value[bot.type].filter((b) => b.target !== bot.target);
-      bots.push(await $api.save(bot));
-      model.value[bot.type] = bots.sort((a, b) => a.target.localeCompare(b.target));
-      return $notify.Icon(msg, 'mdi-content-save', create ? 'green-13' : 'amber-13');
-    } catch (e) {
-      return e;
-    }
-  };
-  return {
-    model,
-    newsBots: computed(() => model.value['news'] as Bots),
-    searchBots: computed(() => model.value['search'] as Bots),
-    sitemapBots: computed(() => model.value['sitemap'] as Bots),
-    create: async (bot: Bot) => save(bot, true),
-    delete: async (type: BotType, target: Target) => {
+      const msg = `${capitalize(bot.type)} Bot ${create ? 'Created' : 'Updated'}`;
       try {
-        const idx = model.value[type].findIndex((b) => b.target === target);
-        if (idx === -1) return;
-        await $api.delete(type, target);
-        model.value[type].splice(idx, 1);
-        return $notify.Icon(`${capitalize(type)} Bot Deleted`, 'mdi-delete', 'pink-13');
+        const bots = model.value[bot.type].filter((b) => b.target !== bot.target);
+        bots.push(await $api.save(bot));
+        model.value[bot.type] = bots.sort((a, b) => a.target.localeCompare(b.target));
+        return $notify.Icon(msg, 'mdi-content-save', create ? 'green-13' : 'amber-13');
       } catch (e) {
         return e;
       }
+    };
+    return {
+      model,
+      newsBots: computed(() => model.value['news'] as Bots),
+      searchBots: computed(() => model.value['search'] as Bots),
+      sitemapBots: computed(() => model.value['sitemap'] as Bots),
+      create: async (bot: Bot) => save(bot, true),
+      delete: async (type: BotType, target: Target) => {
+        try {
+          const idx = model.value[type].findIndex((b) => b.target === target);
+          if (idx === -1) return;
+          await $api.delete(type, target);
+          model.value[type].splice(idx, 1);
+          return $notify.Icon(`${capitalize(type)} Bot Deleted`, 'mdi-delete', 'pink-13');
+        } catch (e) {
+          return e;
+        }
+      },
+      fetch: async () => {
+        try {
+          const [newsBots, searchBots, sitemapBots] = await Promise.all([
+            $api.get('news'),
+            $api.get('search'),
+            $api.get('sitemap'),
+          ]);
+          model.value['news'] = newsBots.sort((a, b) => a.target.localeCompare(b.target));
+          model.value['search'] = searchBots.sort((a, b) => a.target.localeCompare(b.target));
+          model.value['sitemap'] = sitemapBots.sort((a, b) => a.target.localeCompare(b.target));
+        } catch (e) {
+          console.error(e);
+          return e;
+        }
+      },
+      get: (type: BotType, target?: Target): Bot =>
+        model.value[type]?.find((b) => b.target === target) ?? New(type, target),
+      update: async (bot: Bot) => save(bot, false),
+    };
+  },
+  {
+    persist: {
+      debug: true,
+      storage: sessionStorage,
     },
-    fetch: async () => {
-      try {
-        const [newsBots, searchBots, sitemapBots] = await Promise.all([
-          $api.get('news'),
-          $api.get('search'),
-          $api.get('sitemap'),
-        ]);
-        model.value['news'] = newsBots.sort((a, b) => a.target.localeCompare(b.target));
-        model.value['search'] = searchBots.sort((a, b) => a.target.localeCompare(b.target));
-        model.value['sitemap'] = sitemapBots.sort((a, b) => a.target.localeCompare(b.target));
-      } catch (e) {
-        console.error(e);
-        return e;
-      }
-    },
-    get: (type: BotType, target?: Target): Bot =>
-      model.value[type]?.find((b) => b.target === target) ?? New(type, target),
-    update: async (bot: Bot) => save(bot, false),
-  };
-});
+  },
+);
--- src/stores/news.ts
+++ src/stores/news.ts
@@ -18,79 +18,88 @@ type Url = string;
 
 const $api = useNewsApi();
 
-export const useNewsStore = defineStore('news', () => {
-  /* state */
-  const model = ref<Model>({} as Model);
-  const url = ref<Url>('');
-  const topic = ref<Topic>('');
+export const useNewsStore = defineStore(
+  'news',
+  () => {
+    /* state */
+    const model = ref<Model>({} as Model);
+    const url = ref<Url>('');
+    const topic = ref<Topic>('');
 
-  /* getters */
-  const article = computed(
-    () => news(topic.value, url.value).articles?.[url.value] ?? ({} as Article),
-  );
-  const headlines = computed(() => news(topic.value).headlines ?? ([] as Headlines));
-  const post = computed(() => {
-    const a = article.value;
-    return {
-      body: a.body?.map((b) => `<p>${b}</p>`)?.join('') || '',
-      image: a.image,
-      keywords: a.keywords,
-      publishedAt: date.formatDate(decodeTime(a.id), 'YYYY-MM-DD HH:mm'),
-      summary: a.description,
-      tags: [],
-      title: a.title,
-      backlink: a.url,
-    };
-  });
-
-  /* actions */
-  const deleteHeadline = async (t: Topic, u: Url) => {
-    try {
-      const idx = news(t, u).headlines.findIndex((h) => h.url === u) ?? -1;
-      if (idx === -1) return;
-      await $api.deleteHeadline(t, u);
-      news(t).headlines.splice(idx, 1);
-    } catch (e) {
-      return e;
-    }
-  };
-  const fetchArticle = async (t: Topic, u: Url) => {
-    try {
-      news(t, u).articles[u] = await $api.getArticle(t, u);
-    } catch (e) {
-      return e;
-    }
-  };
-  const fetchHeadlines = async (t: Topic) => {
-    try {
-      news(t).headlines = await $api.getHeadlines(t);
-    } catch (e) {
-      return e;
-    }
-  };
-
-  /* helpers */
-  const news = (t: Topic, u?: Url): News => {
-    topic.value = t;
-    url.value = u ?? '';
-    if (!model.value[t]) {
-      model.value[t] = {
-        headlines: [],
-        articles: {} as Articles,
+    /* getters */
+    const article = computed(
+      () => news(topic.value, url.value).articles?.[url.value] ?? ({} as Article),
+    );
+    const headlines = computed(() => news(topic.value).headlines ?? ([] as Headlines));
+    const post = computed(() => {
+      const a = article.value;
+      return {
+        body: a.body?.map((b) => `<p>${b}</p>`)?.join('') || '',
+        image: a.image,
+        keywords: a.keywords,
+        publishedAt: date.formatDate(decodeTime(a.id), 'YYYY-MM-DD HH:mm'),
+        summary: a.description,
+        tags: [],
+        title: a.title,
+        backlink: a.url,
       };
-    }
-    return model.value[t];
-  };
+    });
 
-  return {
-    model,
-    topic,
-    url,
-    article,
-    headlines,
-    post,
-    deleteHeadline,
-    fetchArticle,
-    fetchHeadlines,
-  };
-});
+    /* actions */
+    const deleteHeadline = async (t: Topic, u: Url) => {
+      try {
+        const idx = news(t, u).headlines.findIndex((h) => h.url === u) ?? -1;
+        if (idx === -1) return;
+        await $api.deleteHeadline(t, u);
+        news(t).headlines.splice(idx, 1);
+      } catch (e) {
+        return e;
+      }
+    };
+    const fetchArticle = async (t: Topic, u: Url) => {
+      try {
+        news(t, u).articles[u] = await $api.getArticle(t, u);
+      } catch (e) {
+        return e;
+      }
+    };
+    const fetchHeadlines = async (t: Topic) => {
+      try {
+        news(t).headlines = await $api.getHeadlines(t);
+      } catch (e) {
+        return e;
+      }
+    };
+
+    /* helpers */
+    const news = (t: Topic, u?: Url): News => {
+      topic.value = t;
+      url.value = u ?? '';
+      if (!model.value[t]) {
+        model.value[t] = {
+          headlines: [],
+          articles: {} as Articles,
+        };
+      }
+      return model.value[t];
+    };
+
+    return {
+      model,
+      topic,
+      url,
+      article,
+      headlines,
+      post,
+      deleteHeadline,
+      fetchArticle,
+      fetchHeadlines,
+    };
+  },
+  {
+    persist: {
+      debug: true,
+      storage: localStorage,
+    },
+  },
+);
